6b36f4a2423192b981921d4ed34bcb4bb1fa114a
Juan M.vi <earth001@gmail.com>
2018-10-09 23:33:29 -0300

    Partial update to angular 6

--- src/app/shared/search/search.service.ts
+++ src/app/shared/search/search.service.ts
@@ -1,48 +1,45 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import 'rxjs/add/operator/map';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class SearchService {
-  constructor(private http: Http) {}
+
+  constructor(private http: HttpClient) { }
+
   getAll() {
-    return this.http.get('assets/data/people.json').map((res: Response) => res.json());
+    return this.http.get('assets/data/people.json');
   }
+
   search(q: string): Observable<any> {
     if (!q || q === '*') {
       q = '';
     } else {
       q = q.toLowerCase();
     }
-    return this.getAll().map(data => {
-      const results: any = [];
-      data.map(item => {
-        // check for item in localStorage
-        if (localStorage['person' + item.id]) {
-          item = JSON.parse(localStorage['person' + item.id]);
-        }
-        if (JSON.stringify(item).toLowerCase().includes(q)) {
-          results.push(item);
-        }
-      });
-      return results;
-    });
+    return this.getAll().pipe(
+      map((data: any) => data
+          .map(item => !!localStorage['person' + item.id] ?
+            JSON.parse(localStorage['person' + item.id]) : item)
+          .filter(item => JSON.stringify(item).toLowerCase().includes(q))
+      ));
   }
 
   get(id: number) {
-    return this.getAll().map(all => {
+    return this.getAll().pipe(map((all: any) => {
       if (localStorage['person' + id]) {
         return JSON.parse(localStorage['person' + id]);
       }
-        return all.find(e => e.id === id);
-      });
+      return all.find(e => e.id === id);
+    }));
   }
 
   save(person: Person) {
     localStorage['person' + person.id] = JSON.stringify(person);
   }
-  
 }
 
 export class Address {
